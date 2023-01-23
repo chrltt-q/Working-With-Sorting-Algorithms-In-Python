c1969da4c00b2ecd73ff64c25b3de1247f876ef5
[chrltt-q] <[charlottequezada17@gmail.com]>
2023-01-23 14:39:23 +0800

    def selection_sort

--- Working_With_Selection_Sort.py
+++ Working_With_Selection_Sort.py
@@ -2,3 +2,16 @@ print("******** PROGRAMMED BY ********")
 print("****** Charlotte Quezada ******")
 print("********** BSCOE 2-2 **********")
 print("*** Sir Danilo Madrigalejos ***\n")
+
+
+def selection_sort(my_nums):
+
+    for i in range(9):
+        minpos = i
+        for j in range(i, 10):
+            if my_nums[j] < my_nums[minpos]:
+                minpos = j
+
+        temp = my_nums[i]
+        my_nums[i] = my_nums[minpos]
+        my_nums[minpos] = temp
