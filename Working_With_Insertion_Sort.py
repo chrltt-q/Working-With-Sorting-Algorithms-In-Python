51d914624ff5cd91cfbd2d2f22a67e0eed899fcc
[chrltt-q] <[charlottequezada17@gmail.com]>
2023-01-23 15:16:45 +0800

    def insertion_sort

--- Working_With_Insertion_Sort.py
+++ Working_With_Insertion_Sort.py
@@ -4,3 +4,12 @@ print("********** BSCOE 2-2 **********")
 print("*** Sir Danilo Madrigalejos ***\n")
 
 # This code implements the insertion sort algorithm
+
+
+def insertion_sort(my_nums):
+    for i in range(1, len(my_nums)):
+        j = i
+        while my_nums[j - 1] > my_nums[j] and j > 0:
+            my_nums[j - 1], my_nums[j] = my_nums[j], my_nums[j - 1]
+            j -= 1
+
