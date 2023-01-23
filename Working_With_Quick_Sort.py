a612b7e6dd6c92424efbee7997766562b6b9e2ae
[chrltt-q] <[charlottequezada17@gmail.com]>
2023-01-23 23:24:46 +0800

    def quick_sort

--- Working_With_Quick_Sort.py
+++ Working_With_Quick_Sort.py
@@ -4,3 +4,24 @@ print("********** BSCOE 2-2 **********")
 print("*** Sir Danilo Madrigalejos ***\n")
 
 # This code implements the quick sort algorithm
+
+
+def quick_sort(array, left, right):
+    if left < right:
+        partition_pos = partition(array, left, right)
+        quick_sort(array, left, partition_pos -1)
+        quick_sort(array, partition_pos + 1, right)
+
+
+def partition(array, left, right):
+    i = left
+    j = right
+    pivot = array[right]
+
+    while i < j:
+        while i < right and array[i] < pivot:
+            i += 1
+
+        while j > left and array[j] > pivot:
+            j -= 1
+
