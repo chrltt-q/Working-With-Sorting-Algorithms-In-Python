6fbe8566f4879f51c1829cf09f1495986d024717
[chrltt-q] <[charlottequezada17@gmail.com]>
2023-01-23 15:51:37 +0800

    Merge Sort

--- Working_With_Merge_Sort.py
+++ Working_With_Merge_Sort.py
@@ -12,21 +12,21 @@ def merge_sort(array):
         left_array = array[:len(array)//2]
         right_array = array[len(array)//2:]
 
-    merge_sort(left_array)
-    merge_sort(right_array)
+        merge_sort(left_array)
+        merge_sort(right_array)
 
-    # Merge the left array and right array
-    i = 0  # for the index of left array
-    j = 0  # for the index of right array
-    k = 0  # for merged index of left and right arrays
-    while i < len(left_array) and j < len(right_array):
-        if left_array[i] < right_array[j]:
-            array[k] = left_array[i]
-            i += 1
-        else:
-            array[k] = right_array[j]
-            j += 1
-        k += 1
+        # Merge the left array and right array
+        i = 0  # for the index of left array
+        j = 0  # for the index of right array
+        k = 0  # for merged index of left and right arrays
+        while i < len(left_array) and j < len(right_array):
+            if left_array[i] < right_array[j]:
+                array[k] = left_array[i]
+                i += 1
+            else:
+                array[k] = right_array[j]
+                j += 1
+            k += 1
 
         while i < len(left_array):
             array[k] = left_array[i]
@@ -38,3 +38,10 @@ def merge_sort(array):
             j += 1
             k += 1
 
+
+print("* * * * * * * * * * * * * * * Merge Sort * * * * * * * * * * * * * * *")
+array_test = [46, 17, 16, 53, 83, 7, 13, 62, 44, 14]
+print("Original Array:", array_test, "\n")
+print("Process:")
+merge_sort(array_test)
+print("\nSorted Array Using Merge Sort:", array_test)
