98271f327a77e6e1dc434c5b0410ce32135bddb7
[chrltt-q] <[charlottequezada17@gmail.com]>
2023-01-23 15:00:06 +0800

    def bubble_sort

--- Working_With_Bubble_Sort.py
+++ Working_With_Bubble_Sort.py
@@ -5,3 +5,12 @@ print("*** Sir Danilo Madrigalejos ***\n")
 
 # This code implements the bubble sort algorithm
 
+
+def bubble_sort(my_nums):
+    for i in range(len(my_nums) - 1, 0, -1):
+        for j in range(i):
+            if my_nums[j] > my_nums[j + 1]:
+                temp = my_nums[j]
+                my_nums[j] = my_nums[j + 1]
+                my_nums[j + 1] = temp
+
