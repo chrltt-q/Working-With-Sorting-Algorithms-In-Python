ef7c4940d4362333edbf3bfd806bcac03ac14563
[chrltt-q] <[charlottequezada17@gmail.com]>
2023-01-23 23:58:39 +0800

    quick_sort

--- Working_With_Quick_Sort.py
+++ Working_With_Quick_Sort.py
@@ -8,13 +8,26 @@ print("*** Sir Danilo Madrigalejos ***\n")
 
 def quick_sort(array):
     length = len(array)
+    if length <= 1:
+        return array
+    else:
+        pivot = array.pop()
 
+    greater_elements = []
+    lesser_elements = []
 
+    for item in array:
+        if item > pivot:
+            greater_elements.append(item)
+        else:
+            lesser_elements.append(item)
+
+    print(quick_sort(lesser_elements) + [pivot] + quick_sort(greater_elements))
+    return quick_sort(lesser_elements) + [pivot] + quick_sort(greater_elements)
 
 
 print("* * * * * * * * * * * * * * * Quick Sort * * * * * * * * * * * * * * *")
 my_nums = [46, 17, 16, 53, 83, 7, 13, 62, 44, 14]
 print("Original Array:", my_nums, "\n")
 print("Process:")
-quick_sort(my_nums)
-print("\nSorted Array Using QuickSort:", my_nums)
+print("\nSorted Array Using QuickSort:", quick_sort(my_nums))
