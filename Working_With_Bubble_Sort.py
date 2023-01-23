abb563ea02426205e237425e87ce4adea3dd63a4
[chrltt-q] <[charlottequezada17@gmail.com]>
2023-01-23 14:51:58 +0800

    This code

--- Working_With_Bubble_Sort.py
+++ Working_With_Bubble_Sort.py
@@ -2,3 +2,6 @@ print("******** PROGRAMMED BY ********")
 print("****** Charlotte Quezada ******")
 print("********** BSCOE 2-2 **********")
 print("*** Sir Danilo Madrigalejos ***\n")
+
+# This code implements the bubble sort algorithm
+
