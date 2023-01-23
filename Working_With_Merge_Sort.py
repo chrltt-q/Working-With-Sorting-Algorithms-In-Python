78994a16022da4f29980d1a8ebf664be5ecd31eb
[chrltt-q] <[charlottequezada17@gmail.com]>
2023-01-23 15:30:42 +0800

    def merge_sort

--- Working_With_Merge_Sort.py
+++ Working_With_Merge_Sort.py
@@ -4,3 +4,10 @@ print("********** BSCOE 2-2 **********")
 print("*** Sir Danilo Madrigalejos ***\n")
 
 # This code implements the merge sort algorithm
+
+
+def merge_sort(my_nums):
+    if len(my_nums) > 1:
+        # Divide the array into two sub-arrays called left array and right array
+        left_array = my_nums[:len(my_nums)//2]
+        right_array = my_nums[len(my_nums)//2:]
